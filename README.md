d452bd582422b7f61e0b9da071373e9998f2073b
Bassman2 <ralf.beckers@hotmail.com>
2023-02-18 10:00:44 +0100

    Create

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # setup-SHFB
-This is a Github action for setup EWSoftware/SHFB to your actions.
+
+This is a Github action to install EWSoftware/SHFB for later use.
+
+## Example
+
+    name: "Test"
+
+    on: [push, pull_request]
+
+    jobs:
+      build:
+        runs-on: windows-2022
+    
+        steps:
+        - name: Install checkout
+          uses: actions/checkout@v3
+      
+        - name: Add msbuild to PATH
+          uses: microsoft/setup-msbuild@v1.1
+      
+        - name: Install SHFB
+          uses: Bassman2/setup-SHFB@v1
+          with:
+            version: 2022.12.30.0
+        
+        - name: Create Test Documentation
+          run: msbuild setup-SHFB-Test.sln /p:configuration="Release" /m /verbosity:minimal
+
+## SHFB Versions
+
+The version parameter is optional. If no version is set the latest version will be used.
+
+Currently the following versions can be used:
+
+* 2022.12.30.0 (default)
+* 2022.10.15.0
+* 2022.8.14.1
+
+## Runner
+
+This action can be used for the windows-latest or windows-2022 and windows-2019 runners.
---
+++ setup.ps1
@@ -0,0 +1,48 @@
+<#
+    .SYNOPSIS
+    Install SHFB
+    .DESCRIPTION
+    Install EWSoftware/SHFBCreates for later use
+    .PARAMETER version
+    Version of SHFB to install
+    .NOTES
+    Written by Ralf Beckers
+#>
+
+param(
+    [parameter(Mandatory = $true)]
+    [string]$version
+	)
+	
+$instFolder = Join-Path -Path $PSScriptRoot -ChildPath "SHFBInstaller_$version"	
+$vsixinstaller22 = "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\VSIXInstaller.exe"
+$vsixinstaller19 = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\IDE\VSIXInstaller.exe"
+					
+Write-Host "Install SHFB Version $version"
+
+if (![IO.Directory]::Exists($instFolder))
+{
+	Write-Host "Error: Unknown version"
+	exit -1
+}
+	
+Write-Host "Installing SandcastleHelpFileBuilder.msi..."
+$msi = Join-Path -Path $instFolder -ChildPath "SandcastleHelpFileBuilder.msi"	
+cmd /c start /wait msiexec /i $msi /quiet
+
+if ([IO.File]::Exists($vsixinstaller19))
+{
+	Write-Host "Installing VSIX for VS 2019..."
+	$vsix = Join-Path -Path $instFolder -ChildPath "SHFBVisualStudioPackage_VS2017And2019.vsix"	
+	. $vsixinstaller19 /q /a $vsix
+}
+
+if ([IO.File]::Exists($vsixinstaller22))
+{
+	Write-Host "Installing VSIX for VS 2022..."
+	$vsix = Join-Path -Path $instFolder -ChildPath "SHFBVisualStudioPackage_VS2022AndLater.vsix"	
+	. $vsixinstaller22 /q /a $vsix
+
+}
+
+Write-Host "Sandcastle installed" -ForegroundColor Green	
